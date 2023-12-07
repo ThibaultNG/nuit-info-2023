44be9cc779ebf8d41e1fd15867335e5ad6f76cbd
ThibaultNG <byosoris@gmail.com>
2023-12-07 20:45:00 +0100

    FIX : MainNavBar

--- src/component/MainNavBar.vue
+++ src/component/MainNavBar.vue
@@ -2,24 +2,19 @@ import { RouteItemInfo } from '../router/RouteInfo';
 <template>
 	<v-navigation-drawer :model-value="modelValue">
 		<v-list v-for="route in routes" :key="route.path">
-            <v-list-item :title="route.name" :to="route.path">
-            </v-list-item>
-         
-
-
-        </v-list>
+			<v-list-item :title="(route.name as string)" :to="route.path"/>
+		</v-list>
 	</v-navigation-drawer>
 </template>
 
 <script setup lang="ts">
-import type { RouteRecordRaw } from 'vue-router';
-import routes from '@/router/routes';
+import routes from "@/router/routes";
 
-const props = defineProps<{
+defineProps<{
 	modelValue: boolean;
 }>();
 
-const emit = defineEmits<{
+defineEmits<{
 	(e: "update:modelValue", value: boolean): void;
 }>();
 </script>
