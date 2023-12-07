271c6cfafc846f3b836ed0c46f81c501ec9150ea
ThibaultNG <byosoris@gmail.com>
2023-12-07 19:42:42 +0100

    Change RouteItemInfo interface

--- src/router/RouteInfo.ts
+++ src/router/RouteInfo.ts
@@ -1,16 +1,12 @@
 /* 
 	These interfaces define the links within <MainNavBar/>
-	The "Group" is what expands, revealing the "Items" which are the links
 */
 
-export default interface RouteGroupInfo {
-	groupName: string;
-	groupIcon: string;
-	itemList: RouteItemInfo[];
-}
+import type { RouteRecordRaw } from "vue-router";
 
 export interface RouteItemInfo {
 	name: string;
-	link: string;
+	path: string;
 	icon: string;
+	route: RouteRecordRaw;
 }
