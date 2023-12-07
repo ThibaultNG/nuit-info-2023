6d337079d9168bf1b7210adf828cc0f3717dcb25
ThibaultNG <byosoris@gmail.com>
2023-12-07 21:50:28 +0100

    DEV : Import image from public

--- src/modules/game/component/MainCard.vue
+++ src/modules/game/component/MainCard.vue
@@ -1,4 +1,11 @@
 <template>
-    <v-card class="bg-dark" title="My Card">
-    </v-card>
+	<v-card class="bg-dark" title="My Card" style="max-width: 500px">
+		<img
+			src="/cards/sheep.png"
+			alt="A sheep"
+			style="width: 100%; height: auto; object-fit: contain"
+		/>
+	</v-card>
 </template>
+
+<script setup lang="ts"></script>
