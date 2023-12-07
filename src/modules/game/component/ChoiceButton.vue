8a167d21c094811e9013f61632508354e4f938ed
ThibaultNG <byosoris@gmail.com>
2023-12-08 00:46:00 +0100

    DEV: ChoiceButton

---
+++ src/modules/game/component/ChoiceButton.vue
@@ -0,0 +1,33 @@
+<template>
+	<div style="display: flex; align-items: center;">
+		<v-btn
+			variant="outlined"
+			:text="choiceText"
+            style="flex-grow: 100; margin-right: 10px;"
+			@click="console.log('Droite')"
+		></v-btn>
+		<v-btn variant="text" color="info" icon="mdi-help-circle" @click="dialog = true" />
+		<v-dialog v-model="dialog" transition="dialog-top-transition" width="auto">
+			<v-card>
+				<v-toolbar color="primary" :title="choiceText"></v-toolbar>
+				<v-card-text>
+					{{ prop.description }}
+				</v-card-text>
+				<v-card-actions class="justify-end">
+					<v-btn color="error" variant="tonal" @click="dialog = false">Close</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const dialog = ref<boolean>(false);
+
+const prop = defineProps<{
+	choiceText: string;
+	description: string;
+}>();
+</script>
--- src/modules/game/component/card/MainCard.vue
+++ src/modules/game/component/card/MainCard.vue
@@ -1,24 +1,14 @@
 <template>
-	<v-card class="bg-dark" style="width: 500px;">
+	<v-card class="bg-dark" style="width: 500px">
 		<FlippingCard />
 		<v-container style="display: flex; flex-direction: column">
-			<v-btn
-				variant="outlined"
-				text="Droite"
-				style="width: 100%"
-				class="mb-2"
-				@click="console.log('Droite')"
-			></v-btn>
-			<v-btn
-				variant="outlined"
-				text="Gauche"
-				style="width: 100%"
-				@click="console.log('Gauche')"
-			></v-btn>
+			<ChoiceButton choice-text="Droite" description="Hello world" />
+			<ChoiceButton choice-text="Gauche" description="Noooo" />
 		</v-container>
 	</v-card>
 </template>
 
 <script setup lang="ts">
+import ChoiceButton from "../ChoiceButton.vue";
 import FlippingCard from "./FlippingCard.vue";
 </script>
