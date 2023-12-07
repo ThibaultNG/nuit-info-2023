6a8d65a5dfbc20c0ce1f0659144a3d031063e659
ThibaultNG <byosoris@gmail.com>
2023-12-07 22:20:55 +0100

    DEV: Game buttons

--- src/modules/game/component/MainCard.vue
+++ src/modules/game/component/MainCard.vue
@@ -5,6 +5,21 @@
 			alt="A sheep"
 			style="width: 100%; height: auto; object-fit: contain"
 		/>
+		<v-container style="display: flex; flex-direction: column;">
+			<v-btn
+				variant="outlined"
+				text="Droite"
+				style="width: 100%"
+                class="mb-2"
+				@click="console.log('Droite')"
+			></v-btn>
+			<v-btn
+				variant="outlined"
+				text="Gauche"
+				style="width: 100%"
+				@click="console.log('Gauche')"
+			></v-btn>
+		</v-container>
 	</v-card>
 </template>
 
