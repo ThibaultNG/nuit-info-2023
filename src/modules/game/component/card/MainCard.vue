cf59bff6bcee4dec9bcc6524acaa4263d286b800
ThibaultNG <byosoris@gmail.com>
2023-12-08 04:57:28 +0100

    FIX: Possible null pointer

--- src/modules/game/component/card/MainCard.vue
+++ src/modules/game/component/card/MainCard.vue
@@ -1,32 +1,27 @@
 <template>
-	<v-card class="bg-dark" style="width: 500px">
-		<v-container class="text-h6">
-            {{ card.event }}
-        </v-container>
-		<CardImage :name="card.name" />
-		<v-container style="display: flex; flex-direction: column">
-			<ChoiceButton
-				class="mb-2"
-				:choice-text="card.left.answer"
-				@click="handleClick"
-			/>
-			<ChoiceButton
-				:choice-text="card.right.answer"
-				@click="handleClick"
-			/>
-		</v-container>
-	</v-card>
-	<v-dialog v-model="dialog" transition="dialog-top-transition" width="auto">
-		<v-card>
-			<v-toolbar color="primary" :title="card.event"></v-toolbar>
-			<v-card-text>
-				{{ card.alert }}
-			</v-card-text>
-			<v-card-actions class="justify-end">
-				<v-btn color="error" variant="tonal" @click="dialog = false">Close</v-btn>
-			</v-card-actions>
+	<div v-if="card">
+		<v-card class="bg-dark" style="width: 500px">
+			<v-container class="text-h6">
+				{{ card.event }}
+			</v-container>
+			<CardImage :name="card.name" />
+			<v-container style="display: flex; flex-direction: column">
+				<ChoiceButton class="mb-2" :choice-text="card.left.answer" @click="handleClick" />
+				<ChoiceButton :choice-text="card.right.answer" @click="handleClick" />
+			</v-container>
 		</v-card>
-	</v-dialog>
+		<v-dialog v-model="dialog" transition="dialog-top-transition" width="auto">
+			<v-card>
+				<v-toolbar color="primary" :title="card.event"></v-toolbar>
+				<v-card-text>
+					{{ card.alert }}
+				</v-card-text>
+				<v-card-actions class="justify-end">
+					<v-btn color="error" variant="tonal" @click="dialog = false">Close</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-dialog>
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -40,7 +35,7 @@ const gameStore = useGameStore();
 const card = computed(() => gameStore.currentCard);
 
 function handleClick() {
-	if (card.value.alert) dialog.value = true;
+	if (gameStore.currentCard?.alert) dialog.value = true;
 	gameStore.updateCard();
 }
 </script>
